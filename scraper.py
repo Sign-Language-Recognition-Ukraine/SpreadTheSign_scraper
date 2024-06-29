e76e1766024eda197fb14ae074f08498369538ec
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-06-29 23:33:03 +0300

    scrape ALL word video links

--- scraper.py
+++ scraper.py
@@ -18,9 +18,6 @@
 SCRAPE_LINKS = False
 DOWNLOAD_LINKS = True 
 
-input_dir = "/kaggle/input/cifar-10-fake-image-dataset/cifar-10-dataset/" #! Kaggle path
-working_dir = "/kaggle/working/" #! Kaggle path
-
 # from distutils.dir_util import copy_tree
 # source_dir = "/kaggle/input/spreadthesign-ukrainian-sign-language-videos"
 # destination_dir = "/kaggle/working/"
@@ -32,25 +29,39 @@ from bs4 import BeautifulSoup
 import time
 HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
 
-def scrape_website(url, writer):
-    response = requests.get(url, headers=HEADERS)
+def scrape_website(urlBase, urlExt, currentIndex, writer, recursive, subIndex):
+    time.sleep(0.3)
+    response = requests.get(urlBase+urlExt+currentIndex, headers=HEADERS)
     soup = BeautifulSoup(response.text, 'html.parser')
-
     try:
         src = soup.find('video')['src']
     except TypeError:  
         src = "UNAVAILABLE" 
 
+    if recursive:
+        try:
+            number = 0
+            list = soup.find("div", attrs={"class":"col-md-7"}).find_all('a', attrs={"data-replace":"show-result", "class":"js-replace"})
+            if len(list) != 0:
+                for item in list:
+                    scrape_website(urlBase, f"{item['href']}", "", writer, False, number)
+                    number += 1
+                return
+            
+        except (TypeError, AttributeError):  
+            print("NOT FOUND ")
+    
+
     text = soup.find('h2').text.strip() if soup.find('h2') else "UNAVAILABLE"
 
-    writer.writerow([text,src])
+    writer.writerow([text,src,subIndex])
     
 if SCRAPE_LINKS:
     with open('videos.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        for i in range(1,4958): 
+        for i in range(3196,4958): 
             print(f"{i} ")
-            scrape_website('https://www.spreadthesign.com/uk.ua/word/'+str(i), writer)
+            scrape_website('https://www.spreadthesign.com','/uk.ua/word/', str(i), writer, True, 0)
         
 import urllib.request
 import os
@@ -68,7 +79,7 @@ if DOWNLOAD_LINKS:
             for row in reader:
                 time.sleep(0.3)
                 index += 1
-                text, src = row
+                text, src, subIndex = row
                 if src!= "UNAVAILABLE" and text!= "UNAVAILABLE":
                     unsafe_chars = r'[*/\"<>|@`\']'
                     filename = f"videos/{index}__{re.sub(unsafe_chars, '_', text)}"
@@ -76,10 +87,10 @@ if DOWNLOAD_LINKS:
                         urllib.request.urlretrieve(src, filename)
                         print(f"Downloaded {filename}")
                         
-                        writer.writerow([text, src, filename])
+                        writer.writerow([text, src, subIndex, filename])
                         
                     except Exception as e:
                         print(f"Failed to download {filename}: {e}")
-                        writer.writerow([text, src, "COULD NOT DOWNLOAD"])
+                        writer.writerow([text, src, subIndex, "COULD NOT DOWNLOAD"])
                 else:
-                    writer.writerow([text, src, "UNAVAILABLE"])
+                    writer.writerow([text, src, subIndex, "UNAVAILABLE"])
